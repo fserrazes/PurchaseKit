f52aebbb9bc5a884265f0666c54d58766fcc1932
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-09-09 17:12:23 +0100

    config: Define platforms and minimum requirements

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "PurchaseKit",
+    platforms: [
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
